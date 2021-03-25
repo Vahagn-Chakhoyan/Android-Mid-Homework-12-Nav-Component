9cc0f33d96907112b01a741e0fa434d773868d19
Vahagn Chakhoyan <vahagn.chakhoyan@mail.ru>
2021-03-25 14:32:00 +0400

    completed ex 1

---
+++ app/src/main/java/com/vahagn/android_mid_homework_10/Ex2.kt
@@ -0,0 +1,11 @@
+package com.vahagn.android_mid_homework_10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ex2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ex2)
+    }
+}
--- app/src/main/java/com/vahagn/android_mid_homework_10/MainActivity.kt
+++ app/src/main/java/com/vahagn/android_mid_homework_10/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.vahagn.android_mid_homework_10
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener {
+    private lateinit var toolbar: Toolbar
+    private val exMap = mapOf(R.id.menuEx1 to Ex1::class.java,
+            R.id.menuEx2 to Ex2::class.java,
+            R.id.menuEx3 to Ex3::class.java)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.toolbar)
+        toolbar.setOnMenuItemClickListener(this)
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        item?.let {
+            startActivity(Intent(this, exMap[it.itemId]))
+        }
+        return true
     }
 }
